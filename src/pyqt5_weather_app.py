aab2d44625b715d4b9875bfc20a47b9ac4febca1
SanczoPL <grzegorz600425@gmail.com>
2020-04-04 11:24:45 +0200

    Update MainWindow class

--- src/pyqt5_weather_app.py
+++ src/pyqt5_weather_app.py
@@ -3,15 +3,29 @@
 from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5.QtWidgets import QLabel, QGridLayout
 from PyQt5 import QtSvg
+import json
 
-import widgets.weather_tile
-
+import widgets.weather_tile as weather_tile
+import utilities.config_reader as config_reader
+import utilities.requester as requester
+import utilities.select_day as select_day
 
 class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-
         self.interfejs()
+        config = config_reader.ConfigReader()
+        data = config.load("config/config.json")
+        print(data)
+        appid = data["Api"]["Appid"]
+        url = data["Api"]["Forecasts"]
+        req = requester.Requester(appid,url)
+        dataFromWebApi = req.fetch()
+        print(dataFromWebApi)
+        day_selector = select_day.SelectDay()
+        day = '2020-04-05'
+        selected_dat = day_selector.select(dataFromWebApi,day)
+        print(selected_dat)
 
     def interfejs(self):
         List = []
@@ -19,13 +33,13 @@ class MainWindow(QWidget):
         day = "pt."
         min="-1º"
         max="10º"
-        icon = widgets.weather_tile.WeatherTile(svgName, day,max ,min )
+        icon = weather_tile.WeatherTile(svgName, day,max ,min )
 
         svgName = "../weather-icons-master/svg/wi-day-cloudy.svg"
         day = "sob."
         min = "-2º"
         max = "11º"
-        icon2 = widgets.weather_tile.WeatherTile(svgName, day, max, min)
+        icon2 = weather_tile.WeatherTile(svgName, day, max, min)
 
         firstLayout = QGridLayout()
         svgWidget = QtSvg.QSvgWidget(svgName)
@@ -37,10 +51,8 @@ class MainWindow(QWidget):
         self.show()
 
 
-
 if __name__ == '__main__':
     import sys
-
     app = QApplication(sys.argv)
     okno = MainWindow()
     sys.exit(app.exec_())
