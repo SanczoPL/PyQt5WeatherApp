103cf45e62119c6cbf5e8089f9a81c435d61cce4
SanczoPL <grzegorz600425@gmail.com>
2020-04-04 10:23:12 +0200

    Refactor class

--- src/widgets/weather_tile.py
+++ src/widgets/weather_tile.py
@@ -9,22 +9,24 @@ class WeatherTile(QWidget):
         super().__init__()
         self.svgWidget = QtSvg.QSvgWidget(svgImage)
         self.label = QLabel(labelname, self)
-
-        self.schemeLabelMinMax = QGridLayout()
         self.labelMin = QLabel(min, self)
         self.labelMax = QLabel(max, self)
-        self.schemeLabelMinMax.addWidget(self.labelMax, 0, 0)
-        self.schemeLabelMinMax.addWidget(self.labelMin, 0, 1)
+        # Instance:
+        self.schemeLabelMinMax = QGridLayout()
         self.widgetMinMax = QWidget()
-        self.widgetMinMax.setLayout(self.schemeLabelMinMax)
+        self.scheme = QGridLayout()
         self.init_ui()
 
     def init_ui(self):
-        self.scheme = QGridLayout()
+
+        self.schemeLabelMinMax.addWidget(self.labelMax, 0, 0)
+        self.schemeLabelMinMax.addWidget(self.labelMin, 0, 1)
+        self.widgetMinMax.setLayout(self.schemeLabelMinMax)
         self.scheme.addWidget(self.label, 0, 0)
         self.scheme.addWidget(self.svgWidget, 1, 0)
         self.scheme.addWidget(self.widgetMinMax, 2, 0)
         self.setLayout(self.scheme)
+
         self.setWindowTitle('Burning widget')
         self.setMinimumSize(100, 200)
         self.show()
