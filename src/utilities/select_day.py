f07be31768e96c9900c73148afb7927ab7766744
SanczoPL <grzegorz600425@gmail.com>
2020-04-04 21:58:14 +0200

    Fix date

--- src/utilities/select_day.py
+++ src/utilities/select_day.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import datetime
+from time import gmtime, strftime
 
 
 class SelectDay():
     def __init__(self):
         super().__init__()
 
-    def select(self, data, day):
+    def select(self, data, day): 
         selected_day = []
-
-        print(len(data["list"]))
+        now = strftime("%Y-%m-%d", gmtime())
+        print("Date :{}".format(now))
+        print("Date is in future")
         for i in range(0, len(data["list"])):
             date_time_obj = datetime.datetime.strptime(data["list"][i]["dt_txt"], '%Y-%m-%d %H:%M:%S')
-            print(date_time_obj.day)
             if date_time_obj.date().strftime('%Y-%m-%d') == day:
                 day_temp = []
-                print("good")
-                day_temp.append(((data["list"][i]["dt_txt"]), data["list"][i]["main"]["temp"]))
+                daytime = datetime.datetime.strptime(data["list"][i]["dt_txt"], '%Y-%m-%d %H:%M:%S')
+                day_insert = daytime.strftime('%H')
+                day_temp.append((day_insert, data["list"][i]["main"]["temp"], data["list"][i]["main"]["temp_max"], data["list"][i]["main"]["temp_min"]))
+
                 selected_day.append(day_temp)
         return selected_day
